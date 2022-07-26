2f4e62272f5fb22a346ae96f5b2d095bb44bde8e
ssshivam02 <ssshivamshararma02@gmail.com>
2022-07-26 23:55:08 +0530

    shivamfastapi work

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -8,11 +8,15 @@ router=APIRouter(
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 def vote(vote:schemas.Vote,db:Session=Depends(database.get_db),current_user:int=Depends(oauth2.get_current_user)):
+    post=db.query(models.Post).filter(models.Post.id==vote.post_id).first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Post with id:{vote.post_id} does not exit")
+
     vote_query=db.query(models.Vote).filter(models.Vote.post_id==vote.post_id,models.Vote.user_id==current_user.id)
     found_vote=vote_query.first()
     if (vote.dir==1):
         if found_vote:
-            raise HTTPException(status_code=status.HTTP_409_CONFLICT,detail=f"user {current_user.id} has already voted for voted on post {vote.post_id}")
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT,detail=f"User {current_user.id} has already voted for voted on post {vote.post_id}")
         new_vote=models.Vote(post_id=vote.post_id,user_id=current_user.id)
         db.add(new_vote)
         db.commit()
@@ -23,3 +27,6 @@ def vote(vote:schemas.Vote,db:Session=Depends(database.get_db),current_user:int=
         vote_query.delete(synchronize_session=False)
         db.commit()
         return {"message":"Successfully deleted vote"}
+#for join                               tablefirst.first_second_fk= second.pk
+#select posts.id from posts left join users on posts.owner_id=users.id;
+#we posts.id because of id present in both table(posts and users)
